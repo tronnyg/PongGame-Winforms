76934522047e51ed8f2e5fcb8c02d140c73c712a
Almighty Tronny <tronnyg@gmail.com>
2024-03-01 13:49:00 +0200

    intial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace PongGame
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picPlayer2 = new System.Windows.Forms.PictureBox();
+            this.picPlayer1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picPlayer2
+            // 
+            this.picPlayer2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.picPlayer2.Location = new System.Drawing.Point(1069, 341);
+            this.picPlayer2.Name = "picPlayer2";
+            this.picPlayer2.Size = new System.Drawing.Size(41, 115);
+            this.picPlayer2.TabIndex = 1;
+            this.picPlayer2.TabStop = false;
+            // 
+            // picPlayer1
+            // 
+            this.picPlayer1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.picPlayer1.Location = new System.Drawing.Point(39, 341);
+            this.picPlayer1.Name = "picPlayer1";
+            this.picPlayer1.Size = new System.Drawing.Size(41, 115);
+            this.picPlayer1.TabIndex = 2;
+            this.picPlayer1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.SeaShell;
+            this.pictureBox1.Location = new System.Drawing.Point(397, 252);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 35);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1150, 816);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.picPlayer1);
+            this.Controls.Add(this.picPlayer2);
+            this.Name = "frmMain";
+            this.Text = "Pong";
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picPlayer2;
+        private System.Windows.Forms.PictureBox picPlayer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
